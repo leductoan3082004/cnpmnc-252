642ccb27727341a549b91451f047f71f240499c6
Toan Le <leductoan3082004@gmail.co>
2026-02-25 20:28:33 +0700

    chore: create table before inserting

--- init.sql
+++ init.sql
@@ -1,3 +1,10 @@
+CREATE TABLE IF NOT EXISTS students (
+    id VARCHAR(255) PRIMARY KEY,
+    name VARCHAR(255),
+    email VARCHAR(255),
+    age INTEGER
+);
+
 INSERT INTO students (id, name, email, age) VALUES
     ('1', 'John Doe', 'john.doe@example.com', 20),
     ('2', 'Jane Smith', 'jane.smith@example.com', 22),
